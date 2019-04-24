123a2ec564500517b612d64ac22854cb760d3364
GriKate <kate23april@yandex.ru>
2019-04-24 18:04:46 +0300

    первое дз

---
+++ first-homework.js
@@ -0,0 +1,11 @@
+var tempCels = 10;
+var tempFar = (9/5) * tempCels + 32;
+alert(tempCels + " градусов цельсия соответствует " + tempFar + " градусам по Фаренгейту");
+
+var admin;
+var name = "Василий";
+admin = name;
+alert(admin);
+
+var string = 1000 + "108";
+alert(string);
