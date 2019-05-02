b20db68062aa1e97a7a4413897289387f6d704f0
GriKate <kate23april@yandex.ru>
2019-05-03 01:23:13 +0300

    исправила ошибки

--- homework-2.js
+++ homework-2.js
@@ -12,140 +12,101 @@
 // //3. Объявить две целочисленные переменные a и b и задать им произвольные начальные значения.
 // var a = -1, b = 3;
 //
-// if (a>=0 && b>=0) {
+// if (a >= 0 && b >= 0) {
 //     alert(a - b);
-// } else if(a<0 && b<0) {
+// } else if(a < 0 && b < 0) {
 //     alert(a * b);
-// } else if((a>=0 && b<=0) || (a<=0 && b>=0)) {
-//     alert(a + b);
+// } else {
+//      alert(a + b);
 // }
+//// } else if((a >= 0 && b <= 0) || (a <= 0 && b >= 0)) {
+////     alert(a + b);
+//// }
 
 //. Присвоить переменной а значение в промежутке [0..15].
 // var a = +prompt("Введите число от 0 до 15");
 //
 // switch (a) {
 //     case 0:
-//         while (a <= 15) {
-//             alert(a++);
-//         };
-//         break;
+//         alert(0);
 //     case 1:
-//         while (a <= 15) {
-//             alert(a++);
-//         };
-//         break;
+//         alert(1);
 //     case 2:
-//         while (a <= 15) {
-//             alert(a++);
-//         };
-//         break;
+//         alert(2);
 //     case 3:
-//         while (a <= 15) {
-//             alert(a++);
-//         };
-//         break;
+//         alert(3);
 //     case 4:
-//         while (a <= 15) {
-//             alert(a++);
-//         };
-//         break;
+//         alert(4);
 //     case 5:
-//         while (a <= 15) {
-//             alert(a++);
-//         };
-//         break;
+//         alert(5);
 //     case 6:
-//         while (a <= 15) {
-//             alert(a++);
-//         };
-//         break;
+//         alert(6);
 //     case 7:
-//         while (a <= 15) {
-//             alert(a++);
-//         };
-//         break;
+//         alert(7);
 //     case 8:
-//         while (a <= 15) {
-//             alert(a++);
-//         };
-//         break;
+//         alert(8);
 //     case 9:
-//         while (a <= 15) {
-//             alert(a++);
-//         };
-//         break;
+//         alert(9);
 //     case 10:
-//         while (a <= 15) {
-//             alert(a++);
-//         };
-//         break;
+//         alert(10);
 //     case 11:
-//         while (a <= 15) {
-//             alert(a++);
-//         };
-//         break;
+//         alert(11);
 //     case 12:
-//         while (a <= 15) {
-//             alert(a++);
-//         };
-//         break;
+//         alert(12);
 //     case 13:
-//         while (a <= 15) {
-//             alert(a++);
-//         };
-//         break;
+//         alert(13);
 //     case 14:
-//         while (a <= 15) {
-//             alert(a++);
-//         };
-//         break;
+//         alert(14);
 //     case 15:
-//         while (a <= 15) {
-//             alert(a++);
-//         };
-//         break;
+//         alert(15);
 // }
 
+// while (a <= 15) {
+//             alert(a++);
+//         };
+//         break;
+
 //5. Реализовать основные 4 арифметические операции в виде функций с двумя параметрами.
-// function sum(a, b) {
-//     return alert(a + b);
-// };
+function sum(a, b) {
+    return alert(a + b);
+};
 // var c = sum(2, 3);
 //
-// function subtraction(a, b) {
-//     return alert(a - b);
-// }
+function subtraction(a, b) {
+    return alert(a - b);
+}
 // var c = subtraction(6, 3);
 //
-// function mult(a, b) {
-//     return alert(a * b);
-// }
+function mult(a, b) {
+    return alert(a * b);
+}
 // var c = mult(3, 5);
 //
-// function division(a, b) {
-//     return alert(a / b);
-// }
+function division(a, b) {
+    return alert(a / b);
+}
 // var c = division(20, 5);
 
 //6. Реализовать функцию с тремя параметрами: function mathOperation(arg1, arg2, operation), где arg1, arg2
 function mathOperation(arg1, arg2, operation) {
-    if (arg1>=0 && arg2>=0) {
+    if (arg1 >= 0 && arg2 >= 0) {
         switch (operation) {
             case "-":
-                return alert(arg1 - arg2);
-                break;
+                return subtraction(arg1, arg2);
             case "+":
-                return alert(arg1 + arg2);
-                break;
+                return sum(arg1, arg2);
             case "*":
-                return alert(arg1 * arg2);
-                break;
+                return mult(arg1, arg2);
+            case "/":
+                return division(arg1, arg2);
+            default:
+                alert("Произошла ошибка! Некорректная операция")
         }
     }
 }
 var a = +prompt("Введите первое число:");
 var b = +prompt("Введите второе число:");
-var o = prompt("Введите символ операции (-, +, *):");
+var o = prompt("Введите символ операции (-, +, *, /):");
 var answer = mathOperation(a, b, o);
 
 
